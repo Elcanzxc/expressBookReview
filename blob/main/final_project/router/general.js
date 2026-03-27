6835edfd2ae1a0a588f8cf3bb7565ab72d01e196
Elcanzxc <elcan4361@gmail.com>
2026-03-27 15:30:07 +0400

    Refactor book retrieval methods to use promises

--- blob/main/final_project/router/general.js
+++ blob/main/final_project/router/general.js
@@ -4,88 +4,66 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-// --- Task 10: Get the list of books available in the shop using Async/Await ---
+// --- Task 10: Get all books using Async/Await ---
 public_users.get('/', async function (req, res) {
     try {
-        const getBooks = () => {
-            return new Promise((resolve) => {
-                resolve(books);
-            });
-        };
-        const allBooks = await getBooks();
-        res.status(200).send(JSON.stringify(allBooks, null, 4));
+        const getAllBooks = () => Promise.resolve(books);
+        const result = await getAllBooks();
+        res.status(200).json(result);
     } catch (error) {
-        res.status(500).json({message: "Error retrieving books"});
+        res.status(500).json({message: "Internal Server Error"});
     }
 });
 
-// --- Task 11: Get book details based on ISBN using Promises ---
+// --- Task 11: Get book by ISBN using Promises ---
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    const findBook = new Promise((resolve, reject) => {
+    const findByIsbn = new Promise((resolve, reject) => {
         if (books[isbn]) {
             resolve(books[isbn]);
         } else {
-            reject("Book not found");
+            reject("Book with this ISBN not found");
         }
     });
 
-    findBook
-        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+    findByIsbn
+        .then((book) => res.status(200).json(book))
         .catch((err) => res.status(404).json({message: err}));
 });
 
-// --- Task 12: Get book details based on author using Async/Await ---
+// --- Task 12: Get books by Author using Async/Await ---
 public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
     try {
-        const getByAuthor = () => {
-            return new Promise((resolve) => {
-                let filteredBooks = Object.values(books).filter(b => b.author === author);
-                resolve(filteredBooks);
+        const getBooksByAuthor = () => {
+            return new Promise((resolve, reject) => {
+                const filtered = Object.values(books).filter(b => b.author === author);
+                if (filtered.length > 0) resolve(filtered);
+                else reject("No books found for this author");
             });
         };
-        const result = await getByAuthor();
-        res.status(200).json({booksbyauthor: result});
+        const result = await getBooksByAuthor();
+        res.status(200).json(result);
     } catch (error) {
-        res.status(500).json({message: "Error searching by author"});
+        res.status(404).json({message: error});
     }
 });
 
-// --- Task 13: Get book details based on title using Promises ---
+// --- Task 13: Get books by Title using Promises (С ИСПРАВЛЕНИЕМ ОШИБОК) ---
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-    const getByTitle = new Promise((resolve) => {
-        let filteredBooks = Object.values(books).filter(b => b.title === title);
-        resolve(filteredBooks);
+    const findByTitle = new Promise((resolve, reject) => {
+        const filtered = Object.values(books).filter(b => b.title === title);
+        if (filtered.length > 0) {
+            resolve(filtered);
+        } else {
+            reject("No books found with this title"); // ТЕПЕРЬ ЕСТЬ ОБРАБОТКА ОШИБКИ
+        }
     });
 
-    getByTitle.then((result) => res.status(200).json({booksbytitle: result}));
-});
-
-// Get book review (Остается без изменений)
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    if (books[isbn]) {
-        res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
-    } else {
-        res.status(404).json({message: "Book not found"});
-    }
-});
-
-// Регистрация (Остается без изменений)
-public_users.post("/register", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username && password) {
-        if (!isValid(username)) { 
-            users.push({"username":username,"password":password});
-            return res.status(200).json({message: "Customer successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});    
-        }
-    } 
-    return res.status(404).json({message: "Unable to register user."});
+    findByTitle
+        .then((result) => res.status(200).json(result))
+        .catch((err) => res.status(404).json({message: err})); // Возвращаем 404, если не найдено
 });
 
 module.exports.general = public_users;
